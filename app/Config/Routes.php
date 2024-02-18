6b97e2917d4d24aac3befbdc2b951a03c4a584ba
Chris Dominic P. Chan <chanchrisdominic@gmail.com>
2024-02-18 10:55:46 +0800

    added routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,4 +6,15 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
- $routes->get("/patient/get", "PatientController::index");
+$routes->group("", ["filter" => "ValidateJWTFilter"], function ($routes) {
+    $routes->get("/patient/get/(:num)", "PatientController::index/$1");
+    $routes->get("/doctor/get/(:num)", "DoctorController::index/$1");
+    $routes->get("/patient/record/(:num)/(:num)", "PatientController::get_recent_records/$1/$2");
+
+    $routes->post("/patient/create", "PatientController::create_patient");
+    $routes->post("/patient/record/upload", "PatientController::upload_record");
+    $routes->post("/patient/assign", "PatientController::assign_doctor");
+
+    $routes->post("/doctor/create", "DoctorController::create_doctor");
+    $routes->post("/doctor/login", "DoctorController::login");
+});
