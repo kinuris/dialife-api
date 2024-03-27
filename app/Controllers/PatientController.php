82bb1b6ab437af0c4e8a490c9df0dfc98057bc60
Chris Dominic P. Chan <chanchrisdominic@gmail.com>
2024-03-27 17:32:18 +0800

    added functions

--- app/Controllers/PatientController.php
+++ app/Controllers/PatientController.php
@@ -8,7 +8,7 @@ use App\Models\PatientModel;
 use App\Models\PatientRecordModel;
 use App\Utils\Utils;
 use DateTime;
-use CodeIgniter\HTTP\ResponseInterface;
+use stdClass;
 
 class PatientController extends BaseController
 {
@@ -21,6 +21,16 @@ class PatientController extends BaseController
             ->setContentType("application/json")
             ->setJSON($patientModel->find($id));
     }
+
+    public function all()
+    {
+        $patientModel = new PatientModel();
+
+        return $this->response
+            ->setContentType("application/json")
+            ->setJSON($patientModel->findAll());
+    }
+
 
     public function sync_patient_state()
     {
@@ -74,6 +84,163 @@ class PatientController extends BaseController
             ->setStatusCode(200);
     }
 
+    public function get_latest_record()
+    {
+        $post = $this->request->getJSON();
+
+        if (
+            !isset($post->type) ||
+            !isset($post->patient_id)
+        ) {
+            return $this->response
+                ->setContentType('application/json')
+                ->setJSON(['message' => 'invalid data shape'])
+                ->setStatusCode(404);
+        }
+
+        switch ($post->type) {
+            case "glucose":
+                $createdAtString = "glucose_created_at";
+                $relevantProperties = ['blood_glucose_level', 'glucose_created_at'];
+                break;
+            case "nutrition":
+                $createdAtString = "nutrition_created_at";
+                $relevantProperties = ['nutrition_created_at', 'nutrition_foods', 'nutrition_meal_time'];
+                break;
+            case "activity":
+                $createdAtString = "activity_created_at";
+                $relevantProperties = ['activity_type', 'activity_duration', 'activity_frequency', 'activity_created_at'];
+                break;
+            case "medication":
+                $createdAtString = "medicine_taken_at";
+                $relevantProperties = ['medicine_name', 'medicine_route', 'medicine_form', 'medicine_dosage', 'medicine_taken_at'];
+                break;
+            case "bmi":
+                $createdAtString = "bmi_created_at";
+                $relevantProperties = ['bmi_level', 'bmi_created_at'];
+                break;
+            default:
+                return $this->response
+                    ->setContentType('application/json')
+                    ->setJSON(['message' => 'invalid type passed'])
+                    ->setStatusCode(404);
+        }
+
+        $recordModel = new PatientRecordModel();
+        $record = $recordModel->orderBy($createdAtString, 'DESC')
+            ->where($createdAtString . " IS NOT NULL")
+            ->where('fk_patient_id', $post->patient_id)
+            ->limit(1)
+            ->find();
+
+        $stripped = new stdClass();
+
+        if (count($record) < 1) {
+            return $this->response
+                ->setContentType('application/json')
+                ->setJSON($stripped);
+        }
+
+        $record = $record[0];
+
+        if (isset($record[$createdAtString])) {
+            foreach ($relevantProperties as $property) {
+                $stripped->$property = $record[$property];
+            }
+        }
+
+        return $this->response
+            ->setContentType('application/json')
+            ->setJSON($stripped);
+    }
+
+    public function get_records()
+    {
+        $post = $this->request->getJSON();
+
+        if (
+            !isset($post->type) ||
+            !isset($post->days) ||
+            !isset($post->patient_id)
+        ) {
+            return $this->response
+                ->setContentType('application/json')
+                ->setJSON(['message' => 'invalid data shape'])
+                ->setStatusCode(404);
+        }
+
+        $daysBack = strtotime('-' . $post->days . ' days');
+
+        switch ($post->type) {
+            case "glucose":
+                $createdAtString = "glucose_created_at";
+                $relevantProperties = ['blood_glucose_level', 'glucose_created_at'];
+                break;
+            case "nutrition":
+                $createdAtString = "nutrition_created_at";
+                $relevantProperties = ['nutrition_created_at', 'nutrition_foods', 'nutrition_meal_time'];
+                break;
+            case "activity":
+                $createdAtString = "activity_created_at";
+                $relevantProperties = ['activity_type', 'activity_duration', 'activity_frequency', 'activity_created_at'];
+                break;
+            case "medication":
+                $createdAtString = "medicine_taken_at";
+                $relevantProperties = ['medicine_name', 'medicine_route', 'medicine_form', 'medicine_dosage', 'medicine_taken_at'];
+                break;
+            case "bmi":
+                $createdAtString = "bmi_created_at";
+                $relevantProperties = ['bmi_level', 'bmi_created_at'];
+                break;
+            default:
+                return $this->response
+                    ->setContentType('application/json')
+                    ->setJSON(['message' => 'invalid type passed'])
+                    ->setStatusCode(404);
+        }
+
+        $recordModel = new PatientRecordModel();
+        $records = $recordModel->orderBy($createdAtString, 'DESC')
+            ->where($createdAtString . ' > ' . "'" . date('Y/m/d H:i:s', $daysBack) . "'")
+            ->where('fk_patient_id', $post->patient_id)
+            ->findAll();
+
+        $validRecords = array();
+        foreach ($records as $record) {
+            if (isset($record[$createdAtString])) {
+                $stripped = new stdClass();
+                foreach ($relevantProperties as $property) {
+                    $stripped->$property = $record[$property];
+                }
+
+                array_push($validRecords, $stripped);
+            }
+        }
+
+        $deduped = array();
+        for ($i = 0; $i < count($validRecords); $i++) {
+            if ($i === count($validRecords) - 1) {
+                array_push($deduped, $validRecords[$i]);
+                break;
+            }
+
+            foreach ($relevantProperties as $property) {
+                $currentFields[$property] = $validRecords[$i]->$property;
+                $nextFields[$property] = $validRecords[$i + 1]->$property;
+            }
+
+            if ($currentFields === $nextFields) {
+                continue;
+            }
+
+            array_push($deduped, $validRecords[$i]);
+        }
+
+        return $this->response
+            ->setContentType('application/json')
+            ->setJSON($deduped);
+    }
+
     public function upload_record()
     {
         $post = $this->request->getJSON();
@@ -87,18 +254,15 @@ class PatientController extends BaseController
             // !isset($post->activity_type) ||
             // !isset($post->activity_duration) ||
             // !isset($post->activity_frequency) ||
+            // !isset($post->nutrition_foods) ||
             // !isset($post->nutrition_created_at) ||
-            // !isset($post->nutrition_protein) ||
-            // !isset($post->nutrition_fat) ||
-            // !isset($post->nutrition_carbohydrates) ||
-            // !isset($post->nutrition_water) ||
+            // !isset($post->nutrition_meal_time) ||
             // !isset($post->medicine_name) ||
             // !isset($post->medicine_route) ||
             // !isset($post->medicine_form) ||
             // !isset($post->medicine_dosage) ||
             // !isset($post->medicine_taken_at) ||
             !isset($post->patient_id)
-
         ) {
             return $this->response
                 ->setContentType('application/json')
@@ -117,31 +281,29 @@ class PatientController extends BaseController
         if (getenv("CI_ENVIRONMENT") === 'development') {
             date_default_timezone_set("Asia/Manila");
         }
-        
+
         if (isset($latest)) {
             $latest = new DateTime($latest["created_at"]);
             $interval = $latest->diff(new DateTime());
             $total_minutes = ($interval->days * 24 * 60) + ($interval->h * 60) + $interval->i;
 
-            if ($total_minutes < 10) {
-                $remaining = 10 - $total_minutes;
-                return $this->response
-                    ->setContentType('application/json')
-                    ->setJSON(["timeout_duration_minutes" => "$remaining"])
-                    ->setStatusCode(200);
-            }
+            // if ($total_minutes < 10) {
+            //     $remaining = 10 - $total_minutes;
+            //     return $this->response
+            //         ->setContentType('application/json')
+            //         ->setJSON(["timeout_duration_minutes" => "$remaining"])
+            //         ->setStatusCode(200);
+            // }
         }
 
         $recordModel->insert([
-            'blood_glucose_level' => $post->glucose_level,
+            'blood_glucose_level' => $post->blood_glucose_level,
             'bmi_level' => $post->bmi_level,
             'activity_type' => $post->activity_type,
             'activity_duration' => $post->activity_duration,
             'activity_frequency' => $post->activity_frequency,
-            'nutrition_protein' => $post->nutrition_protein,
-            'nutrition_fat' => $post->nutrition_fat,
-            'nutrition_carbohydrates' => $post->nutrition_carbohydrates,
-            'nutrition_water' => $post->nutrition_water,
+            'nutrition_foods' => $post->nutrition_foods,
+            'nutrition_meal_time' => $post->nutrition_meal_time,
             'fk_patient_id' => $post->patient_id,
             'glucose_created_at' => $post->glucose_created_at,
             'bmi_created_at' => $post->bmi_created_at,
@@ -207,10 +369,18 @@ class PatientController extends BaseController
     public function revoke_doctor()
     {
         // TODO: Both patients and doctors can do this
+        $jwt = get_cookie("jwt");
+
+        if (!isset($jwt) || $jwt === "deleted") {
+            return $this->response
+                ->setContentType('application/json')
+                ->setJSON(['message' => 'Unauthorized'])
+                ->setStatusCode(403);
+        }
 
         $post = $this->request->getJSON();
 
-        if (!isset($post->connection_id)) {
+        if (!isset($post->doctor_id) || !isset($post->patient_id)) {
             return $this->response
                 ->setContentType('application/json')
                 ->setJSON([
@@ -220,22 +390,24 @@ class PatientController extends BaseController
         }
 
         $connectionModel = new ConnectionModel();
-        $connection = $connectionModel->find($post->connection_id);
-        if (!isset($connection)) {
-            return $this->response
-                ->setContentType('application/json')
-                ->setJSON(['message' => 'connection not found'])
-                ->setStatusCode(404);
-        }
+        // $connection = $connectionModel->find($post->connection_id);
+        // if (!isset($connection)) {
+        //     return $this->response
+        //         ->setContentType('application/json')
+        //         ->setJSON(['message' => 'connection not found'])
+        //         ->setStatusCode(404);
+        // }
 
-        $connectionModel->delete($post->connection_id);
+        $connectionModel->where('fk_doctor_id', $post->doctor_id)
+            ->where('fk_patient_id', $post->patient_id)
+            ->delete();
 
         return $this->response
             ->setContentType('application/json')
             ->setJSON([
                 'message' => 'Success',
-                'doctor_id' => $connection['fk_doctor_id'],
-                'patient_id' => $connection['fk_patient_id'],
+                'doctor_id' => $post->doctor_id,
+                'patient_id' => $post->patient_id,
             ])
             ->setStatusCode(200);
     }
