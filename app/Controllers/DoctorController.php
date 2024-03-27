060500b5a5d91d19fc59cdfd0c11d86c785698c1
Chris Dominic P. Chan <chanchrisdominic@gmail.com>
2024-03-27 17:32:06 +0800

    added functions

--- app/Controllers/DoctorController.php
+++ app/Controllers/DoctorController.php
@@ -3,8 +3,11 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\ConnectionModel;
 use App\Models\DoctorModel;
 use App\Models\DoctorNumberModel;
+use App\Models\PatientModel;
+use App\Models\RegistrationKeyModel;
 use App\Utils\Utils;
 use Firebase\JWT\JWT;
 
@@ -13,10 +16,54 @@ class DoctorController extends BaseController
     public function index($id)
     {
         $doctorModel = new DoctorModel();
+        $doctor = $doctorModel->find($id);
+        unset($doctor['password']);
 
         return $this->response
             ->setContentType("application/json")
-            ->setJSON($doctorModel->find($id));
+            ->setJSON($doctor);
+    }
+
+    public function gen_registration_keys()
+    {
+        $post = $this->request->getJSON();
+
+        if (
+            !isset($post->count) ||
+            !isset($post->secret)
+        ) {
+            return $this->response
+                ->setContentType('application/json')
+                ->setJSON(['message' => 'invalid data shape'])
+                ->setStatusCode(404);
+        }
+
+        $keys = array();
+        for ($i = 0; $i < $post->count; $i++) {
+            $key = implode('-', str_split(substr(strtolower(md5(microtime() . rand(1000, 9999))), 0, 30), 6));
+            array_push($keys, ['key_string' => $key]);
+        }
+
+        $keyModel = new RegistrationKeyModel();
+        $keyModel->insertBatch($keys);
+
+        return $this->response
+            ->setContentType('application/json')
+            ->setJSON($keys);
+    }
+
+    public function check_auth()
+    {
+        $jwt = get_cookie("jwt");
+
+        if (!isset($jwt) || $jwt === "deleted") {
+            return $this->response
+                ->setContentType('application/json')
+                ->setJSON(['message' => 'Unauthorized'])
+                ->setStatusCode(403);
+        }
+
+        return $this->response->setStatusCode(200);
     }
 
     public function add_contact()
@@ -106,8 +153,42 @@ class DoctorController extends BaseController
             ->setStatusCode(200);
     }
 
+    public function get_connected_patients($doctorId)
+    {
+        $jwt = get_cookie("jwt");
+
+        if (!isset($jwt) || $jwt === "deleted") {
+            return $this->response
+                ->setContentType('application/json')
+                ->setJSON(['message' => 'Unauthorized'])
+                ->setStatusCode(403);
+        }
+
+        $connectionModel = new ConnectionModel();
+        $connections = $connectionModel->where("fk_doctor_id", $doctorId)->findAll();
+
+        $patients = array();
+        foreach ($connections as $connection) {
+            $patientModel = new PatientModel();
+            $patient = $patientModel->find($connection['fk_patient_id']);
+            array_push($patients, $patient);
+        }
+
+        return $this->response
+            ->setContentType('application/json')
+            ->setJSON($patients);
+    }
+
     public function get_numbers()
     {
+        $jwt = get_cookie("jwt");
+
+        if (!isset($jwt) || $jwt === "deleted") {
+            return $this->response
+                ->setContentType('application/json')
+                ->setJSON(['message' => 'Unauthorized'])
+                ->setStatusCode(403);
+        }
         // TODO: Add to routes
         // TODO: Check for patient doctor connection
         $post = $this->request->getJSON();
@@ -133,7 +214,7 @@ class DoctorController extends BaseController
             ->setJSON($numbers);
     }
 
-    public function login() 
+    public function login()
     {
         $doctorModel = new DoctorModel();
         $post = $this->request->getJSON();
@@ -185,19 +266,44 @@ class DoctorController extends BaseController
 
     public function create_doctor()
     {
-        $doctorModel = new DoctorModel();
         $post = $this->request->getJSON();
 
         if (
             !isset($post->name) ||
             !isset($post->email) ||
-            !isset($post->password)
+            !isset($post->password) ||
+            !isset($post->regkey)
         ) {
             return $this->response
                 ->setContentType('application/json')
                 ->setJSON(['message' => 'invalid data shape'])
                 ->setStatusCode(404);
         }
+
+        $keyModel = new RegistrationKeyModel();
+        $key = $keyModel->where('key_string', $post->regkey)
+            ->find();
+
+        if (count($key) < 1 || (int)$key[0]['used']) {
+            return $this->response
+                ->setContentType('application/json')
+                ->setJSON(['message' => 'Unauthorized'])
+                ->setStatusCode(403);
+        }
+
+        $doctorModel = new DoctorModel();
+        $existingEmail = $doctorModel->where('email', $post->email)
+            ->find();
+
+        if (count($existingEmail) > 0) {
+            return $this->response
+                ->setContentType('application/json')
+                ->setJSON(['message' => 'email already exists'])
+                ->setStatusCode(404);
+        }
+
+        $key = $keyModel->where('key_string', $post->regkey)->find(); 
+        $keyModel->update($key[0]['registration_key_id'], ['used' => true]);
 
         $doctorModel->insert([
             'name' => $post->name,
